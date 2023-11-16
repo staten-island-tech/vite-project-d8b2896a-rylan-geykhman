9be149bbfb4b74f5fe9c7b5baeabf2f3d4dda94b
rylan-geykhman <144476581+rylan-geykhman@users.noreply.github.com>
2023-11-16 13:14:37 -0500

    commit

--- project/js/main.js
+++ project/js/main.js
@@ -1,40 +1,29 @@
-import './style.css';
+import '../style.css';
 import { data } from "./games";
 
 const DOMSelectors = {
-    form: document.querySelector(".form"),
     container: document.querySelector(".container"),
-    gameName: document.getElementById("name"),
-    gamePrice: document.getElementById("price"),
+    cardlist:document.querySelector(".cardlist")
 };
 
-//function create() {
-    //const thing = {
-        //names: DOMSelectors.gameName.value,
-        //pictures: DOMSelectors.picture.value,
-        //price: DOMSelectors.gamePrice.value,
-    //};
-//}
-
-/*function insert(thing, DOMSelectors) {
-    DOMSelectors.container.insertAdjacentHTML("afterbegin", 
-    ` <div class="card">
-        <h2 class="card-title">${thing.names}</h2>
-            <img src="${thing.pictures}" alt="picture" class="card-img">
-        <h3 class="card-emails">${thing.price}</h3>
-    </div>`);
-}
-DOMSelectors.container.addEventListener("load", function (event) {
-    event.preventDefault()
-    create()
-});*/
 data.forEach((game)=>{
     const newObject = document.createElement("div");
-    newObject.container.insertAdjacentHTML("afterbegin",
-        ` <div class="card">
-        <h2 class="card-title">${thing.names}</h2>
-        <img src="${thing.pictures}" alt="picture" class="card-img">
-        <h3 class="card-emails">${thing.price}</h3>
-        </div>`)
-    
+    newObject.innerHTML =
+        ` 
+        <h2 class="card-title">${game.name}</h2>
+        <img src="${game.picture}" alt="picture" class="card-img">
+        <h3 class="card-price">${game.price}</h3>
+        `
+    newObject.classList.add("card")
+    DOMSelectors.cardlist.appendChild(newObject)
+});
+
+function freeGame(){
+    data.filter((game)=> game.price.value == "true");
+    DOMSelectors.cardlist.appendChild(newObject)
+}
+
+DOMSelectors.cardlist.addEventListener("click", function (event) {
+    event.preventDefault()
+    freeGame()
 });
