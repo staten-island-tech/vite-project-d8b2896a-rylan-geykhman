2754eadd34aa8797cb40b0658fce7b200cab5163
rylan-geykhman <144476581+rylan-geykhman@users.noreply.github.com>
2023-11-22 12:25:26 -0500

    commit

--- project/js/main.js
+++ project/js/main.js
@@ -13,6 +13,8 @@ DOMSelectors.button.addEventListener("click", function (event) {
     freeGame()
 });
 
+
+
 data.forEach((game)=>{
     const newObject = document.createElement("div");
     newObject.innerHTML =
@@ -25,4 +27,20 @@ data.forEach((game)=>{
     DOMSelectors.cardlist.appendChild(newObject)
 });
 
-
+function freeGame(){
+    let cards = document.querySelectorAll(".card");
+    console.log(cards)
+    cards.forEach((card)=> 
+            card.parentElement.remove());
+    const FreeGames = data.forEach((game)=>{
+        game.filter((price)=> price.value === "Free")
+    })
+    FreeGames.forEach((game)=>{
+    const anotherObject = document.createElement("div");
+    DOMSelectors.container.insertAdjacentHTML(beforeend,`<h2 class="card-title">${game.name}</h2>
+    <img src="${game.picture}" alt="picture" class="card-img">
+    <h3 class="card-price">${game.price}</h3>
+    `)
+    anotherObject.classList.add("card")
+    DOMSelectors.cardlist.appendChild(anotherObject)
+})};
