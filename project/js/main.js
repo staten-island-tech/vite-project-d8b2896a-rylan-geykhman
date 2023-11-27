a4d688fe2f1d85e6aef70787b7117b214eff4f72
rylan-geykhman <144476581+rylan-geykhman@users.noreply.github.com>
2023-11-27 13:15:38 -0500

    commit

--- project/js/main.js
+++ project/js/main.js
@@ -10,37 +10,52 @@ const DOMSelectors = {
 
 DOMSelectors.button.addEventListener("click", function (event) {
     event.preventDefault()
-    freeGame()
+    shooterGame()
 });
 
+function createCards(){
+    data.forEach((game)=>{
+        const newObject = document.createElement("div");
+        newObject.innerHTML =
+            ` 
+            <h2 class="card-title">${game.name}</h2>
+            <img src="${game.picture}" alt="picture" class="card-img">
+            <h3 class="card-price">${game.price}</h3>
+            `
+        newObject.classList.add("card")
+        DOMSelectors.cardlist.appendChild(newObject)
+})}; 
 
-
-data.forEach((game)=>{
-    const newObject = document.createElement("div");
-    newObject.innerHTML =
-        ` 
-        <h2 class="card-title">${game.name}</h2>
-        <img src="${game.picture}" alt="picture" class="card-img">
-        <h3 class="card-price">${game.price}</h3>
-        `
-    newObject.classList.add("card")
-    DOMSelectors.cardlist.appendChild(newObject)
-});
-
-function freeGame(){
+function shooterGame(){
     let cards = document.querySelectorAll(".card");
     console.log(cards)
     cards.forEach((card)=> 
             card.parentElement.remove());
-    const FreeGames = data.forEach((game)=>{
-        game.filter((price)=> price.value === "Free")
-    })
-    FreeGames.forEach((game)=>{
-    const anotherObject = document.createElement("div");
-    DOMSelectors.container.insertAdjacentHTML(beforeend,`<h2 class="card-title">${game.name}</h2>
-    <img src="${game.picture}" alt="picture" class="card-img">
-    <h3 class="card-price">${game.price}</h3>
-    `)
-    anotherObject.classList.add("card")
-    DOMSelectors.cardlist.appendChild(anotherObject)
-})};
+    const ShooterGames = data.filter((type)=> type.genres.includes("Shooter"))
+    console.log(ShooterGames)
+    ShooterGames.forEach((game)=>{
+        const newObject = document.createElement("div");
+        newObject.innerHTML =
+            ` 
+            <h2 class="card-title">${game.name}</h2>
+            <img src="${game.picture}" alt="picture" class="card-img">
+            <h3 class="card-price">${game.price}</h3>
+            `
+        newObject.classList.add("card")
+        DOMSelectors.cardlist.appendChild(newObject)
+})
+}
+
+/* function createCards(data){
+    const newObject = document.createElement("div");
+    data.forEach((game)=> newObject.insertAdjacentHTML(
+        "beforeend",
+        `<div class="card"> 
+        <h2 class="card-title">${game.name}</h2>
+        <img src="${game.picture}" alt="picture" class="card-img">
+        <h3 class="card-price">${game.price}</h3>
+        </div>`
+    ))
+} */
+
+createCards(data)
